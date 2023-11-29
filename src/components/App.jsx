37e0c9f1839334d492bcba0f9cdd551bcfd5546f
Mariusz-72 <mmbwdap@gmail.com>
2023-11-29 17:59:09 +0100

    update 2.5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react';
 import MyContacts from './Contacts/MyContacts';
 
 const App = () => {
-  // Przykładowy stan, możesz dostosować go do swoich potrzeb
+  
   const [contacts, setContacts] = useState([
     { id: 1, name: 'John Doe', phone: '123-456-7890' },
     { id: 2, name: 'Jane Doe', phone: '987-654-3210' },
-    // Dodaj inne kontakty według potrzeb
+    
   ]);
 
-  // Funkcja do dodawania nowego kontaktu
+  
   const addContact = (newContact) => {
     setContacts((prevContacts) => [...prevContacts, newContact]);
   };
 
   return (
     <div>
-      {/* Przekazujemy stan i funkcję do dodawania kontaktów do komponentu MyContacts */}
+      
       <MyContacts contacts={contacts} addContact={addContact} />
     </div>
   );
