c4e9a8fe34b0632bcf342175b120fb42c2199734
Mariusz-72 <mmbwdap@gmail.com>
2023-11-29 17:36:39 +0100

    update 2.2

--- src/components/Contacts/MyContacts.jsx
+++ src/components/Contacts/MyContacts.jsx
@@ -1,5 +1,5 @@
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
-import { Component } from 'react';
 
 import MyContactForm from 'components/MyContactForm/MyContactForm';
 import MyContactList from 'components/ContactList/ContactList';
@@ -7,98 +7,76 @@ import MyContactsFilter from 'components/MyContactsFilter/MyContactsFilter';
 
 import css from './MyContacts.module.css';
 
-class MyContacts extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-  // Ładowanie kontaktów z localStorage po zamontowaniu komponentu
-  componentDidMount() {
+const MyContacts = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
     const savedContacts = localStorage.getItem('contacts');
     if (savedContacts) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
+      setContacts(JSON.parse(savedContacts));
     }
-  }
-  // Zapisywanie kontaktów do localStorage po każdej zmianie w stanie
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  }, []); // Pusta tablica dependency, useEffect wykona się tylko przy montowaniu komponentu
 
-  addContact = ({ name, number }) => {
-    if (this.isDuplicate(name)) {
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]); // useEffect wykona się za każdym razem, gdy contacts zostanie zaktualizowany
+
+  const addContact = ({ name, number }) => {
+    if (isDuplicate(name)) {
       return alert(`${name} is already in your contacts`);
     }
 
-    this.setState(prevState => {
-      const { contacts } = prevState;
-      const newContact = {
-        id: nanoid(),
-        name,
-        number,
-      };
-
-      return { contacts: [newContact, ...contacts], name: '', number: '' };
-    });
+    setContacts(prevContacts => [
+      { id: nanoid(), name, number },
+      ...prevContacts,
+    ]);
   };
 
-  handleFilter = ({ target }) => {
-    this.setState({ filter: target.value });
+  const handleFilter = ({ target }) => {
+    setFilter(target.value);
   };
 
-  removeContact = id => {
-    this.setState(({ contacts }) => {
-      const newContact = contacts.filter(contact => contact.id !== id);
-      return { contacts: newContact };
-    });
-  };
-
-  isDuplicate(name) {
-    const normalizedName = name.toLowerCase();
-    const { contacts } = this.state;
-    const result = contacts.find(({ name }) => {
-      return name.toLowerCase() === normalizedName;
-    });
-
-    return Boolean(result);
-  }
-
-  getFilteredContacts() {
-    const { filter, contacts } = this.state;
-
-    if (!filter) {
-      return contacts;
-    }
-
-    const normalizedName = filter.toLowerCase();
-    const result = contacts.filter(({ name }) => {
-      return name.toLowerCase().includes(normalizedName);
-    });
-    return result;
-  }
-
-  render() {
-    const { addContact, removeContact, handleFilter } = this;
-    const contacts = this.getFilteredContacts();
-
-    return (
-      <div>
-        <div className={css.phone_part}>
-          <h2 className={css.title}>Phonebook</h2>
-          <MyContactForm onSubmit={addContact} />
-          <MyContactsFilter
-            handleChange={handleFilter}
-            value={this.state.filter}
-          />
-        </div>
-        <div className={css.contacts_list}>
-          <h2 className={css.title}>Contacts</h2>
-          <MyContactList removeContact={removeContact} contacts={contacts} />
-        </div>
-      </div>
+  const removeContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
     );
-  }
-}
+  };
+
+  const isDuplicate = name => {
+    const normalizedName = name.toLowerCase();
+    return contacts.some(
+      contact => contact.name.toLowerCase() === normalizedName
+    );
+  };
+
+  const getFilteredContacts = () => {
+    const normalizedName = filter.toLowerCase();
+    return filter
+      ? contacts.filter(contact =>
+          contact.name.toLowerCase().includes(normalizedName)
+        )
+      : contacts;
+  };
+
+  const filteredContacts = getFilteredContacts();
+
+  return (
+    <div>
+      <div className={css.phone_part}>
+        <h2 className={css.title}>Phonebook</h2>
+        <MyContactForm onSubmit={addContact} />
+        <MyContactsFilter handleChange={handleFilter} value={filter} />
+      </div>
+      <div className={css.contacts_list}>
+        <h2 className={css.title}>Contacts</h2>
+        <MyContactList
+          removeContact={removeContact}
+          contacts={filteredContacts}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default MyContacts;
--- src/components/MyContactForm/MyContactForm.jsx
+++ src/components/MyContactForm/MyContactForm.jsx
@@ -1,85 +1,75 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './MyContactForm.module.css';
 
-class MyContactForm extends Component {
-  state = {
+const MyContactForm = ({ onSubmit }) => {
+  const [formData, setFormData] = useState({
     name: '',
     number: '',
-  };
+  });
 
-  componentDidMount() {
-    // Pobieranie danych z localStorage po zamontowaniu komponentu
+  useEffect(() => {
     const savedFormData = localStorage.getItem('formData');
     if (savedFormData) {
-      this.setState(JSON.parse(savedFormData));
+      setFormData(JSON.parse(savedFormData));
     }
-  }
+  }, []); // Pusta tablica dependency, useEffect wykona się tylko przy montowaniu komponentu
 
-  componentDidUpdate(prevProps, prevState) {
-    // Zapisywanie danych formularza do localStorage po każdej zmianie w stanie
-    if (prevState !== this.state) {
-      localStorage.setItem('formData', JSON.stringify(this.state));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('formData', JSON.stringify(formData));
+  }, [formData]); // useEffect wykona się za każdym razem, gdy formData zostanie zaktualizowane
 
-  handleChange = ({ target }) => {
+  const handleChange = ({ target }) => {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    setFormData(prevFormData => ({ ...prevFormData, [name]: value }));
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { onSubmit } = this.props;
-    onSubmit({ ...this.state });
-    this.setState({
+    onSubmit({ ...formData });
+    setFormData({
       name: '',
       number: '',
     });
   };
 
-  render() {
-    const { handleChange, handleSubmit } = this;
-    const { name, number } = this.state;
+  return (
+    <div className={css.phonebook_wrapper}>
+      <form className={css.phonebook_form} onSubmit={handleSubmit}>
+        <label className={css.phonebook_name}>Name</label>
+        <input
+          className={css.input}
+          onChange={handleChange}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Pablo, Pablo Esteban, Pablo d'Esteban"
+          required
+          value={formData.name}
+          placeholder="  contact name"
+        />
 
-    return (
-      <div className={css.phonebook_wrapper}>
-        <form className={css.phonebook_form} onSubmit={handleSubmit}>
-          <label className={css.phonebook_name}>Name</label>
-          <input
-            className={css.input}
-            onChange={handleChange}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Pablo, Pablo Esteban, Pablo d'Esteban"
-            required
-            value={name}
-            placeholder="  contact name"
-          />
+        <label className={css.phonebook_number}>Number</label>
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          placeholder="  contact number"
+          value={formData.number}
+          onChange={handleChange}
+        />
 
-          <label className={css.phonebook_number}>Number</label>
-          <input
-            className={css.input}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            placeholder="  contact number"
-            value={number}
-            onChange={handleChange}
-          />
-
-          <button className={css.button} type="submit">
-            Add contact
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+        <button className={css.button} type="submit">
+          Add contact
+        </button>
+      </form>
+    </div>
+  );
+};
 
 MyContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
